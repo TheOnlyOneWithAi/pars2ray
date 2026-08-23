467dbba652d0e238bc84f3d8125b059c5d5e2690
theonlyone <parsahoseini549@gmail.com>
2026-08-23 19:22:14 +0330

    feat: register advanced control-plane API

--- master/app/main.py
+++ master/app/main.py
@@ -11,6 +11,7 @@ from fastapi.staticfiles import StaticFiles
 from sqlalchemy import text
 from starlette.responses import PlainTextResponse
 
+from app.api.advanced_control import router as advanced_control_router
 from app.api.ai_autopilot import router as ai_autopilot_router
 from app.api.ai_config import router as ai_config_router
 from app.api.client_manager import router as client_manager_router
@@ -119,6 +120,7 @@ app.include_router(user_provisioning_router)
 app.include_router(ai_settings_router)
 app.include_router(ai_config_router)
 app.include_router(ai_autopilot_router)
+app.include_router(advanced_control_router)
 app.include_router(subscription_router)
 app.include_router(router)
 app.include_router(protocols_router)
